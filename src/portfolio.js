0750e683ab3c6ded7454873acf2920896706b642
Gayathri Balasubramani <gayathribalasubramani@Gayathris-MacBook-Air.local>
2020-10-09 22:39:57 +0100

    create basic app with name

---
+++ src/portfolio.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+const Portfolio = () => {
+  return (
+    <div className="portfolio">
+      <header className="portfolio-header">
+        <p>
+          GAYATHRI BALASUBRAMANI
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Portfolio;
